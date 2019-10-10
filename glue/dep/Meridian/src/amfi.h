526a301dff8172c93cf412ca357ca29f23c368ba
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-09 23:57:33 -0400

    Update amfi.h
    work in progress

--- glue/dep/Meridian/src/amfi.h
+++ glue/dep/Meridian/src/amfi.h
@@ -37,3 +37,24 @@ int hash_code_directory(const CS_CodeDirectory *directory, uint8_t hash[CS_CDHAS
 const char *get_hash_name(uint8_t hash_type);
 int open_img(img_info_t* info);
 void close_img(img_info_t* info);
+
+
+// Not fully sure if any of ther other things in here is needed for supports ios 11, I'll check out later.
+#ifndef apfs_util_h
+#define apfs_util_h
+
+#define get_dirfd(vol) open(vol, O_RDONLY, 0)
+
+const char *find_snapshot_with_ref(const char *vol, const char *ref);
+const char *find_system_snapshot(const char *rootfsmnt);
+
+int do_create(const char *vol, const char *snap);
+int do_delete(const char *vol, const char *snap);
+int do_revert(const char *vol, const char *snap);
+int do_rename(const char *vol, const char *snap, const char *nw);
+int do_mount(const char *vol, const char *snap, const char *mntpnt);
+int list_snapshots(const char *vol);
+int check_snapshot(const char *vol, const char *snap);
+char *copyBootHash(void);
+
+#endif /* apfs_util_h */
